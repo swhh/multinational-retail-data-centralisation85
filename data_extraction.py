3cacd393839419df3952ee70f74b36c3801182a6
swhh <seamus.holland1@gmail.com>
2024-06-18 11:04:54 +0100

    add new methods to utils and extraction

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,20 @@
-
+import pandas as pd
+from database_utils import DatabaseConnector
+CREDS = 'db_creds.yaml'
 
 class DataExtractor:
-    pass
+
+    def read_rds_table(self, db_conn, table_name):
+        """extract the database table to a pandas DataFrame."""
+        with db_conn.engine.connect() as conn:
+            df = pd.read_sql_table(table_name, db_conn.engine)
+        return df
+    
+
+
+if __name__ == '__main__':
+    conn = DatabaseConnector(CREDS)
+    extractor = DataExtractor() 
+    df = extractor.read_rds_table(conn, 'legacy_users')
+    # print(df.head())
+    # print(df.info())
--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,44 @@
+import yaml
+from sqlalchemy import create_engine, inspect
 
+
+
+ENGINE_ADDRESS = "{RDS_DATABASE}://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/postgres"
 
 class DatabaseConnector:
-    pass
+
+    def __init__(self, yaml_file):
+        creds = self.read_db_creds(yaml_file)
+        self.engine = self.init_db_engine(creds)
+
+    def read_db_creds(self, yaml_file):
+        """Read dc_creds.yaml and return creds dictionary"""
+        with open(yaml_file) as f:
+            creds_dict = yaml.safe_load(f)
+        return creds_dict
+    
+    def init_db_engine(self, creds_dict):
+        """read the credentials from read_db_creds 
+        and initialise and return an sqlalchemy database engine"""
+        engine_address = ENGINE_ADDRESS.format(**creds_dict)
+        engine = create_engine(engine_address)
+        return engine
+    
+    def list_tables(self, engine):
+        """list all the tables in the database"""
+        inspector = inspect(engine)
+        return inspector.get_table_names()
+    
+    def upload_to_db(self, df, table_name):
+        """take in a Pandas DataFrame and table name to upload to as an argument."""
+        with self.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+            df.to_sql(table_name, self.engine, if_exists='replace')
+
+    
+    
+if __name__ == '__main__':
+    db_conn = DatabaseConnector()
+    creds_dict = db_conn.read_db_creds('db_creds.yaml')
+    engine = db_conn.init_db_engine(creds_dict)
+    print(db_conn.list_tables(engine))
+    
